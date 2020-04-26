daf670c6509b8151ff2b5760235b7de51e1b5cd6
Hamed Gorjiara <hgorjiar@uci.edu>
2020-04-26 00:54:36 -0700

    Fixing nasty linker buggit add API/pmcheckapi.h

--- API/pmcheckapi.h
+++ API/pmcheckapi.h
@@ -12,10 +12,10 @@ extern "C" {
 #else
 typedef int bool;
 #endif
-void pmc_load8(const void *addr);
-void pmc_load16(const void *addr);
-void pmc_load32(const void *addr);
-void pmc_load64(const void *addr);
+void pmc_load8(void *addr);
+void pmc_load16(void *addr);
+void pmc_load32(void *addr);
+void pmc_load64(void *addr);
 
 void pmc_store8(void *addr);
 void pmc_store16(void *addr);
@@ -23,9 +23,9 @@ void pmc_store32(void *addr);
 void pmc_store64(void *addr);
 
 // PMC NVM operations
-void pmc_clwb(char * addrs);
-void pmc_clflushopt(char * addrs);
-void pmc_clflush(char * addrs);
+void pmc_clwb(void * addrs);
+void pmc_clflushopt(void * addrs);
+void pmc_clflush(void * addrs);
 void pmc_mfence();
 void pmc_lfence();
 void pmc_sfence();
