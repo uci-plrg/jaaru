aa1a64573f4cf63c5932b415a6fb5fbbbb1984eb
Hamed Gorjiara <hgorjiar@uci.edu>
2020-06-11 01:18:16 -0700

    adding a new test case for datarace detection, and non-SC memory order

---
+++ Test/testaquire.cc
@@ -0,0 +1,49 @@
+#include <atomic>
+#include <pthread.h>
+#include <stdio.h>
+#include <cstdlib>
+#include "test.h"
+
+using namespace std;
+#define NUMTHREADS 2
+
+atomic<unsigned int> x(0);
+atomic<unsigned int> y(0);
+
+void* func1(void *){
+    x=1;
+	y=3;
+    x.store(5, std::memory_order_release);
+	cacheOperation(CLWB, (char *)&x, sizeof(unsigned int));
+    cacheOperation(CLWB, (char *)&y, sizeof(unsigned int));
+	mfence();
+    printf("Func2: Value Y= %u\n", y.load());
+    return NULL;
+}
+
+void* func2(void *)
+{
+    while(x.load(std::memory_order_acquire) <= 5);
+	y.store(2, std::memory_order_relaxed);
+    y.store(4, std::memory_order_relaxed);
+	cacheOperation(CLWB, (char*)&y, sizeof(unsigned int));
+	mfence();
+	printf("Func2: Value Y= %u\n", y.load());
+    return NULL;
+}
+
+
+int main(){
+    pthread_t threads[NUMTHREADS];
+	void *(*funcptr[])(void *) = {func1, func2};
+	for (int i=0;i< NUMTHREADS;i++) {
+        if( int retval = pthread_create(&threads[i], NULL, funcptr[i], NULL ) ) {
+            fprintf(stderr, "Unable to create a pthread. Return value %d\n", retval);
+            return EXIT_FAILURE;
+        }
+    }
+    for(int i=0;i< NUMTHREADS;i++) {
+		pthread_join(threads[i], NULL);
+	}
+	return EXIT_SUCCESS;
+}
