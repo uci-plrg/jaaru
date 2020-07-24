5d5e2512b40e951c5dd936e8d384ef35ddd903ad
Hamed Gorjiara <hgorjiar@uci.edu>
2020-07-23 20:15:10 -0700

    Adding test for int**

---
+++ Test/testpointer.cc
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <cstdio>
+#include <malloc.h>
+#define CACHE_LINE_SIZE 64
+typedef struct Node
+{
+    int value;
+    volatile Node *next;
+} Node;
+
+int main()
+{
+    volatile Node* b = (Node *) memalign(CACHE_LINE_SIZE, sizeof(Node));
+    b->value = 22;
+    b->next = NULL;
+    volatile Node* a = (Node *) memalign(CACHE_LINE_SIZE, sizeof(Node));
+    a->value = 11;
+    a->next = b;
+    volatile Node * bcopy = a->next;
+}
