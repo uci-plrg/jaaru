3074e0927073c33d797488ec73a150d047e14cf7
Hamed Gorjiara <hgorjiar@uci.edu>
2021-03-17 23:50:35 -0700

    New test case: Multi cache line breaks Jaaru's address alignment.

--- Test/testrace.cc
+++ Test/testrace.cc
@@ -2,6 +2,9 @@
 #include<stdio.h>
 #include "test.h"
 
+//#define TEST1
+//#define TEST2
+//#define MULTICACHELINE
 extern "C" {
     void * getRegionFromID(uint ID);
     void setRegionFromID(uint ID, void *ptr);
@@ -10,7 +13,9 @@ extern "C" {
 typedef struct testStruct
 {
     uint32_t a = 0;
-    //char nope [64];
+#ifdef TEST1
+    char nope [64];
+#endif
     uint32_t b = 0;
 } testStruct;
 
@@ -19,13 +24,21 @@ int main()
     testStruct *myobj;
     if (getRegionFromID(0) == NULL)
     {
-        myobj = (testStruct *)malloc(sizeof(testStruct) + 192);
+        myobj = (testStruct *)malloc(sizeof(testStruct) + 64 + 64 + 64);
+#ifdef MULTICACHELINE
+        myobj = (testStruct *)(((uintptr_t) myobj & ~(64 - 1)) + 64 + 60);
+#else
+        myobj = (testStruct *)(((uintptr_t) myobj & ~(64 - 1)) + 64);
+#endif
         setRegionFromID(0, myobj);
         myobj->a = 1;
-        cacheOperation(CLFLUSHOPT, (char *)myobj, sizeof(myobj));        
+#ifdef TEST2
+        cacheOperation(CLFLUSH, (char *)myobj, sizeof(myobj));
+#else
+        cacheOperation(CLWB, (char *)myobj, sizeof(myobj));
+#endif
         myobj->b = 2;
         mfence();
-        free(myobj);
     }
     else
     {
