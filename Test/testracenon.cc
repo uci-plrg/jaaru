f997f811dfe8d844e687b6a08d44bb9189b660a5
Hamed Gorjiara <hgorjiar@uci.edu>
2021-03-17 18:35:56 -0700

    test case without persistency race works.

--- Test/testracenon.cc
+++ Test/testracenon.cc
@@ -3,6 +3,8 @@
 #include<atomic>
 #include "test.h"
 
+#define TEST1
+
 extern "C" {
     void * getRegionFromID(uint ID);
     void setRegionFromID(uint ID, void *ptr);
@@ -11,9 +13,11 @@ extern "C" {
 typedef struct testStruct
 {
     testStruct(): a(0), b(0), x(0) {}
-    uint32_t a;
-    uint32_t b;
+    atomic<uint32_t> a;
+    atomic<uint32_t> b;
+#ifdef TEST1
     char nope [64];
+#endif
 	atomic<uint32_t> x;
 } testStruct;
 
@@ -26,9 +30,9 @@ int main()
         //Make sure a,b,x are in the same cache line
         myobj = (testStruct *)(((uintptr_t) myobj & ~(64 - 1)) + 64);
         setRegionFromID(0, myobj);
-        myobj->a = 1;
-        cacheOperation(CLFLUSHOPT, (char *)myobj, sizeof(myobj));        
-        myobj->b = 2;
+        myobj->a.store(1, std::memory_order_relaxed);
+        cacheOperation(CLFLUSH, (char *)myobj, sizeof(myobj));        
+        myobj->b.store(2, std::memory_order_acq_rel);
 		myobj->x.store(5, std::memory_order_release);
         mfence();
     }
@@ -36,7 +40,7 @@ int main()
     {
         myobj = (testStruct *)getRegionFromID(0);
 		printf("atomic x = %d\n", myobj->x.load());
-        printf("a=%u\n", myobj->a);
+        printf("a=%u\n", myobj->a.load());
     }
     return EXIT_SUCCESS;
 }
