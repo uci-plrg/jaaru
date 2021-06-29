3b7cc81e8fb766b94d6ce579aa3dc5309abe3829
Hamed Gorjiara <hgorjiar@uci.edu>
2021-06-29 12:45:53 -0700

    fix(general): read being NULL in FATAL function

--- Plugins/pmverifier.cc
+++ Plugins/pmverifier.cc
@@ -127,7 +127,7 @@ void PMVerifier::recordProgress(ModelExecution *exec, ModelAction *action) {
 		ASSERT(cv);
 		thread_id_t tid = int_to_id(i);
 		if(range->getEndRange()<cv->getClock(tid)){
-			FATAL(exec, action, NULL, "End range %u at Location=%s, and address %p, is not compatable with new begin range %u\n", range->getEndRange(), action->get_position(), action->get_location(), cv->getClock(tid));
+			FATAL(exec, action, action, "End range %u is not compatable with new begin range %u\n", range->getEndRange(), cv->getClock(tid));
 		}
 		range->mergeBeginRange(cv->getClock(tid));
 	}
@@ -217,7 +217,7 @@ void PMVerifier::updateThreadsEndRangeafterWrite(ModelExecution *execution, Mode
 		if(nextWrite) {
 			Range *range = getOrCreateRange(ranges, i);
 			if(range->getBeginRange()>nextWrite->get_seq_number()-1){
-				FATAL(execution, wrt, NULL, "Begin range %u at location %s, and address %p, is not compatable with new end range %u\n", range->getBeginRange(), nextWrite->get_position(), nextWrite->get_location(), nextWrite->get_seq_number()-1);
+				FATAL(execution, wrt, wrt, "Begin range %u is not compatable with new end range %u\n", range->getBeginRange(), nextWrite->get_position(), nextWrite->get_location(), nextWrite->get_seq_number()-1);
 			}
 			range->minMergeEndgeRange(nextWrite->get_seq_number()-1);
 		}
