4e0ffd93b05e37d63bb31f5d04894044386b7800
Hamed Gorjiara <hgorjiar@uci.edu>
2021-08-10 14:35:43 -0700

    fix(general): not showing error if location/library didn't exist

--- Scripts/parse-trace.sh
+++ Scripts/parse-trace.sh
@@ -10,7 +10,7 @@ do
 		continue;
 	fi
 	TRACE=$(echo "$line" | sed -e 's/^[ \t]*//')
-	LIB="$( cut -d '(' -f 1 <<< "$TRACE" )"; 
+	LIB="$( cut -d '(' -f 1 <<< "$TRACE" )";
 	REST="$( cut -d '(' -f 2- <<< "$TRACE" )";
 	FUNC="$( cut -d '+' -f 1 <<< "$REST" )";
 	REST="$( cut -d '+' -f 2- <<< "$REST" )";
@@ -19,7 +19,12 @@ do
 	then
 		LOCATION=$(addr2line -a -f --exe=$LIB $OFFSET | sed -z 's/\n/:/g;s/:$/\n/')
 	else
-		funcaddr=$(nm $LIB | grep $FUNC | head -n1 | cut -d " " -f1) && funcoffset=$(python -c "print hex(0x${funcaddr}+$OFFSET)") && LOCATION=$(addr2line -e $LIB ${funcoffset} | sed -z 's/\n/:/g;s/:$/\n/')
+		funcaddr=$(nm $LIB 2> /dev/null | grep $FUNC 2> /dev/null | head -n1 | cut -d " " -f1 ) && funcoffset=$(python -c "print hex(0x${funcaddr}+$OFFSET)" 2> /dev/null) && LOCATION=$(addr2line -e $LIB ${funcoffset} | sed -z 's/\n/:/g;s/:$/\n/')
+		if test -z "$funcaddr"
+		then
+			echo "$line"
+			continue;
+		fi
 	fi
 	echo "	$LIB:$FUNC($LOCATION)"
 done < "$input"
